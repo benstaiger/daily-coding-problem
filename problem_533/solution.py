67002f30e1580aa7c6bed33941d6a1d43ee8c4f1
bstaiger <bhstaiger@gmail.com>
2022-03-26 10:57:44 -0500

    solve daily problem 533 in pseudocode

---
+++ problem_533/solution.py
@@ -0,0 +1,24 @@
+
+# This problem was asked by Facebook.
+# 
+# Boggle is a game played on a 4 x 4 grid of letters. The goal is to find as
+# many words as possible that can be formed by a sequence of adjacent letters
+# in the grid, using each cell at most once. Given a game board and a
+# dictionary of valid words, implement a Boggle solver.
+
+
+def boggle(grid, dictionary):
+    # starting at each position, do a DFS where we traverse the boggle grid
+    # as well as a Trie formed from all words in our dictionary.
+
+    # honestly...that's a lot to implement in a 30-45 minute coding problem.
+    # Googling, it seems like I am right that this is the best solution.
+    pass
+
+
+def test_boggle():
+    pass
+
+
+if __name__ == "__main__":
+    test_boggle()
