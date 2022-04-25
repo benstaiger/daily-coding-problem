62e2b0075695826017ed6a382639b38fe1830d5e
bstaiger <bhstaiger@gmail.com>
2022-04-25 10:02:36 -0500

    format

--- problem_558/src/main.rs
+++ problem_558/src/main.rs
@@ -1,13 +1,13 @@
 use rand::prelude::*;
 
 // This problem was asked by Google.
-// 
+//
 // The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a Monte Carlo method.
-// 
+//
 // Hint: The basic equation of a circle is x2 + y2 = r2.
 
 fn in_circle(x: f64, y: f64, r: f64) -> bool {
-    x*x + y*y < r*r
+    x * x + y * y < r * r
 }
 
 fn estimate_pi() -> f64 {
@@ -17,7 +17,6 @@ fn estimate_pi() -> f64 {
     let mut samples = 0.0;
     let mut num_in_circle = 0.0;
     let mut rng = rand::thread_rng();
-    let mut estimate = 3.0;
     loop {
         let x: f64 = rng.gen();
         let y: f64 = rng.gen();
@@ -32,7 +31,7 @@ fn estimate_pi() -> f64 {
             break;
         }
     }
-    4.0 * num_in_circle / samples  // 4.0 is the area of a square with side length 2.
+    4.0 * num_in_circle / samples // 4.0 is the area of a square with side length 2.
 }
 
 fn main() {
