6fcf75bc9567ba7244fcde45fda82e82416ee300
bstaiger <bhstaiger@gmail.com>
2022-04-25 10:01:26 -0500

    solve daily problem 561

---
+++ problem_561/src/main.rs
@@ -0,0 +1,31 @@
+// This problem was asked by Etsy.
+//
+// Given a sorted array, convert it into a height-balanced binary search tree.
+
+#[derive(Debug)]
+struct Tree {
+    value: i32,
+    left: Option<Box<Tree>>,
+    right: Option<Box<Tree>>,
+}
+
+fn height_balance(sorted: &[i32]) -> Option<Box<Tree>> {
+    fn height_balance_help(sorted: &[i32], left: usize, right: usize) -> Option<Box<Tree>> {
+        if left > right {
+            return None;
+        }
+
+        let middle = (left + right) / 2;
+        Some(Box::new(Tree {
+            value: sorted[middle],
+            left: if middle > 0 { height_balance_help(sorted, left, middle - 1) } else { None },
+            right: height_balance_help(sorted, middle + 1, right),
+        }))
+    }
+
+    height_balance_help(sorted, 0, sorted.len() - 1)
+}
+
+fn main() {
+    println!("{:?}", height_balance(&vec![0, 1, 2, 3, 4, 5, 6]));
+}
