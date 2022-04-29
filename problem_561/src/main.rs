cd941ef8fefa036ccdf18728766af56dc9c68f1c
bstaiger <bhstaiger@gmail.com>
2022-04-29 07:56:42 -0500

    add assertions

--- problem_561/src/main.rs
+++ problem_561/src/main.rs
@@ -1,3 +1,4 @@
+
 // This problem was asked by Etsy.
 //
 // Given a sorted array, convert it into a height-balanced binary search tree.
@@ -18,7 +19,11 @@ fn height_balance(sorted: &[i32]) -> Option<Box<Tree>> {
         let middle = (left + right) / 2;
         Some(Box::new(Tree {
             value: sorted[middle],
-            left: if middle > 0 { height_balance_help(sorted, left, middle - 1) } else { None },
+            left: if middle > 0 {
+                height_balance_help(sorted, left, middle - 1)
+            } else {
+                None
+            },
             right: height_balance_help(sorted, middle + 1, right),
         }))
     }
@@ -27,5 +32,9 @@ fn height_balance(sorted: &[i32]) -> Option<Box<Tree>> {
 }
 
 fn main() {
-    println!("{:?}", height_balance(&vec![0, 1, 2, 3, 4, 5, 6]));
+    let result = height_balance(&vec![0, 1, 2, 3, 4, 5, 6]).unwrap();
+    println!("{:?}", &result);
+    assert_eq!(result.value, 3);
+    assert_eq!(result.left.unwrap().value, 1);
+    assert_eq!(result.right.unwrap().value, 5);
 }
